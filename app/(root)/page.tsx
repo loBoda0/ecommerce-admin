8b6ea8e32eb9ed420fa52b9fdfd6147fe335b9fe
Martin <martin.loboda02@gmail.com>
2023-07-05 17:01:51 +0200

    init+auth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useStoreModal } from '@/hooks/useStoreModal';
+import { useEffect } from 'react';
+
+
+const Home = () => {
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen])
+  return (
+    <div className='p-4'>
+      Root Page
+    </div>
+  )
+}
+
+export default Home;
